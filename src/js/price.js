1930d2e986701b30359d7b138fa544edb071cca7
Nastenka <Nastenka>
2017-12-14 17:16:56 +0200

    upd

--- src/js/price.js
+++ src/js/price.js
@@ -16,6 +16,7 @@
 
   includedWrap.html('');
   extraWrap.html('');
+  item.html('');
   $('.yw-price__value').text(currentText);
   // $('.yw-price__pic').css('background-image', `url(${currentImg})`);
   for(var i = 0; i < currentIncludedCont.length; i++){
@@ -24,21 +25,32 @@
   for(var i = 0; i < currentExtraCont.length; i++){
     extraWrap.append('<li>'+currentExtraCont[i]+'</li>');
   }
-  for(var i = 0; i < item.length; i++){
-    var text = currentMulti[i].text;
-    var date = currentMulti[i].date;
-    var price = currentMulti[i].price;
-    var current = currentMulti[i].current;
+  if(typeof(currentMulti[1]) != 'undefined'){
+    for(var i = 0; i < currentMulti.length; i++){
+      var text = currentMulti[i].text;
+      var date = currentMulti[i].date;
+      var price = currentMulti[i].price;
+      var current = currentMulti[i].current;
 
-    $(item[i]).find('.yw-price__item-text').text(text);
-    $(item[i]).find('.yw-price__item-date').text(date);
-    $(item[i]).find('.yw-price__item-value').text(price);
-    if(current == 'y'){
-      $(item[i]).addClass('active');
+      $(item[i]).append('<div class="yw-price__item-text">' + text + '</div>');
+      $(item[i]).append('<div class="yw-price__item-date">' + date + '</div>');
+      $(item[i]).append('<div class="yw-price__item-value">' + price + '</div>');
+      if(current == 'y'){
+        $(item[i]).addClass('active');
+      }
     }
   }
 
+
   $('.js-price label').click(function(e){
+    // var fondy_price = $(this).parents('.js-price').data('fprice');
+    // var fondy_title = $(this).parents('.js-price').data('ftitle');
+    // var _id = $(this).parents('.js-price').data('id');
+    // $('#selected_id').val(_id);
+    // $('.filled').text(fondy_price);
+    // $('select.select-product option:selected').attr('data-id',_id).attr('value',fondy_price).text(fondy_title);
+    // $('div.select-product button>span').text(fondy_title);
+    // calculateFondySum();
 
     var target = $(e.currentTarget);
 
@@ -54,9 +66,10 @@
 
     includedWrap.html('');
     extraWrap.html('');
+    item.html('');
     $('.yw-price__value').text(text);
     item.each(function(){
-      $(this).removeClass('active');
+      $(this).removeClass('active show-before');
     });
     //$('.yw-price__pic').css('background-image', `url(${img})`);
     for(var i = 0; i < includedCont.length; i++){
@@ -65,19 +78,24 @@
     for(var i = 0; i < extraCont.length; i++){
       extraWrap.append('<li>'+extraCont[i]+'</li>');
     }
-    for(var i = 0; i < item.length; i++){
-      var text = multi[i].text;
-      var date = multi[i].date;
-      var price = multi[i].price;
-      var current = multi[i].current;
+    if(typeof(multi[1]) != 'undefined'){
+      for(var i = 0; i < item.length; i++){
+        var text = multi[i].text;
+        var date = multi[i].date;
+        var price = multi[i].price;
+        var current = multi[i].current;
 
-      $(item[i]).find('.yw-price__item-text').text(text);
-      $(item[i]).find('.yw-price__item-date').text(date);
-      $(item[i]).find('.yw-price__item-value').text(price);
-      if(current == 'y'){
-        $(item[i]).addClass('active');
+        $(item[i]).addClass('show-before');
+
+        $(item[i]).append('<div class="yw-price__item-text">' + text + '</div>');
+        $(item[i]).append('<div class="yw-price__item-date">' + date + '</div>');
+        $(item[i]).append('<div class="yw-price__item-value">' + price + '</div>');
+        if(current == 'y'){
+          $(item[i]).addClass('active');
+        }
       }
     }
+
   })
 
 })();
